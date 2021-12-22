be6e8dc29249a26e86454c09617be357d9357982
Walber Florêncio <59696629+walberfa@users.noreply.github.com>
2021-12-22 19:30:43 -0300

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,16 @@
+public class Cliente {
+  public static void main(String[] args) {
+    Mario mario = new Mario();
+    mario.pegarCogumelo();
+    mario.pegarPena();
+    mario.levarDano();
+    mario.pegarFlor();
+    mario.pegarFlor();
+    mario.levarDano();
+    mario.levarDano();
+    mario.pegarPena();
+    mario.levarDano();
+    mario.levarDano();
+    mario.levarDano();
+  }
+}
---
+++ MarioMorto.java
@@ -0,0 +1,23 @@
+public class MarioMorto implements MarioState {
+
+	@Override
+	public MarioState pegarCogumelo() {
+		return null;
+	}
+
+	@Override
+	public MarioState pegarFlor() {
+		return null;
+	}
+
+	@Override
+	public MarioState pegarPena() {
+		return null;
+	}
+
+	@Override
+	public MarioState levarDano() {
+		return null;
+	}
+
+}
